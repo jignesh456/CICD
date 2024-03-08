0e0796a9642ce9b0243073c6f01530f00cd5e071
jignesh456 <panchaljignesh494@gmail.com>
2024-03-08 13:09:08 +0530

    Your commit message

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace PracticeWebApp
+{
+    public class Startup
+    {
+        [Obsolete] //If compiler error then true if warning error then false
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("Privacy.cshtml");
+            app.UseDefaultFiles(options);
+
+            app.UseStaticFiles();
+
+            //try
+            //{
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("hello India!");
+            });
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.Write(ex.ToString());   
+            //}
+
+
+        }
+    }
+
+    
+}
